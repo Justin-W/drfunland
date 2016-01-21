58d8fa16d9dcc73f23b15580c1368d5d3b39e4d1
Justin W <justin.webster@gmail.com>
2016-01-21 14:56:23 -0800

    transitions_common: Tests: added (mostly empty) test modules (as placeholders for future tests).

---
+++ drfunapp/transitions_common/test/test_transitions_extensions.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+# from ..transitions_extensions import Machine  # , BetterFSMGraph, BetterPGVGraph, summarize_machine
+
+
+class TestMachineClass(TestCase):
+    """
+    Tests the transitions_extensions.Machine class.
+    """
+    pass
---
+++ drfunapp/transitions_common/test/test_utils.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+# from ..utils import MachineCatalog  # , LogUtils, MimeUtils, kwargs_merge, stringify
+
+
+class TestMachineCatalogClass(TestCase):
+    """
+    Tests the utils.MachineCatalog class.
+    """
+    pass
